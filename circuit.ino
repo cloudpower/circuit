8b8077d77dcabdc5f5d1ae67550c31c1c6bfdbda
Carl Stubens <carl.stubens@gmail.com>
2013-03-18 17:29:52 -0700

    Read in lines. No parsing yet.

---
+++ circuit.ino
@@ -0,0 +1,51 @@
+
+
+const int NUM_PORTS = 2;
+const int IN_BUFF_LEN = 100;
+
+
+void setup() {
+	Serial.begin(9600);
+}
+
+void loop() {
+	char in_buff[IN_BUFF_LEN] = {0};
+	int buff_index = 0;
+	int overflow = 0;
+	char c;
+
+	for(;;) {
+		// Add new characters to a buffer
+		if(Serial.available()) {
+			c = Serial.read();
+
+			// If the buffer is full
+			if(buff_index == IN_BUFF_LEN) {
+				buff_index = 0;
+				overflow = 1;
+			}
+
+			// Add new character to buffer, if it has not overflowed
+			if(overflow == 0) {
+				in_buff[buff_index] = c;
+				buff_index++;
+			}
+
+			// Detect newline character
+			if(c == '\r' || c == '\n') {
+				if(overflow) {
+					// Ignore this overflowed line.
+					// Now that this overflowed line has ended, 
+					// reset the overflow flag.
+					overflow = 0;
+				} else if(buff_index-1 < IN_BUFF_LEN) {
+					// Replace the '\n' with null terminator
+					in_buff[buff_index-1] = 0;
+					// Parse this line as a command
+					handle_input_line(in_buff);
+				}	
+				buff_index = 0;
+			}
+		}
+	}
+}
