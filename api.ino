cba85e4c38f1aabead7b5e92bca0a538ee843ea6
Carl Stubens <carl.stubens@gmail.com>
2013-03-18 17:31:25 -0700

    Parse API commands.

--- api.ino
+++ api.ino
@@ -1,5 +1,71 @@
 
 
+const int SET_MODE_RELAY = 0;
+const int SET_MODE_LED = 1;
+
+
 void handle_input_line(char * line) {
 	Serial.println("handle_input_line");
+
+	if(String(line) == String("report")) {
+		_handle_report();
+
+	} else if(String(line).startsWith("set ")) {
+		_handle_set(line + 4, SET_MODE_RELAY);
+
+	} else if(String(line).startsWith("led ")) {
+		_handle_set(line + 4, SET_MODE_LED);
+	}
+}
+
+void _handle_report() {
+	// TODO
+	Serial.println("_handle_report is not yet implemented");
+}
+
+int _handle_set(char * pairs, int mode) {
+	Serial.println("_handle_set");
+	//Serial.println(pairs);
+	int tok_end = 0;
+
+	for(int i = 0; i < IN_BUFF_LEN; i++) {
+		if(pairs[tok_end] == 0) {
+			_handle_pair(pairs, mode);
+			break;
+		} else if(pairs[tok_end] == ' ') {
+			pairs[tok_end] = 0;
+			_handle_pair(pairs, mode);
+			pairs = pairs + tok_end+1;
+			tok_end = 0;
+		} else {
+			tok_end++;
+		}
+	}
+
+	return 0;
+}
+
+int _handle_pair(char * pair, int mode) {
+	Serial.println("_handle_pair");
+	//Serial.println(pair);
+	int socket;
+	int state;
+	for(int i = 0; i < strlen(pair); i++) {
+		if(pair[i] == ':') {
+			pair[i] = 0;
+			socket = atoi(pair);
+			state = atoi(pair + i+1);
+
+			switch(mode) {
+			case SET_MODE_RELAY:
+				set_relay(socket, state);
+				break;
+			case SET_MODE_LED:
+				set_led(socket, state);
+				break;
+			}
+		}
+	}
+	
+	return 0;
 }
---
+++ power.ino
@@ -0,0 +1,40 @@
+
+
+int get_relay(int socket) {
+	// TODO
+	Serial.print("get_relay not implemented. ");
+	Serial.println(socket);
+	return 0;
+}
+
+int set_relay(int socket, int status) {
+	// TODO
+	Serial.print("set_relay not implemented. ");
+	Serial.print(socket);
+	Serial.print(" ");
+	Serial.println(status);
+	return 0;
+}
+
+int get_led(int socket) {
+	// TODO
+	Serial.print("get_led not implemented. ");
+	Serial.println(socket);
+	return 0;
+}
+
+int set_led(int socket, int status) {
+	// TODO
+	Serial.print("set_led not implemented. ");
+	Serial.print(socket);
+	Serial.print(" ");
+	Serial.println(status);
+	return 0;
+}
+
+int measure_power(int socket) {
+	// TODO
+	Serial.println("measure_power not implemented. ");
+	Serial.println(socket);
+	return 0;
+}
